c0f39475368282ca160590f9f499655ac9652aed
wakhiddw <wakhiddw@gmail.com>
2026-09-13 08:33:59 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='tvtoko-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
